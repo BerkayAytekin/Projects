0742d1d94ef1e1eee883354609a4ca76f0fb632d
BerkayAytekin <146004509+BerkayAytekin@users.noreply.github.com>
2024-05-15 21:46:14 +0300

    Three Column Preview Card Component project

---
+++ Newbie-Level-Projects/three-column-preview-card-component/README.md
@@ -0,0 +1,1 @@
+# three_column_preview_card_component
---
+++ Newbie-Level-Projects/three-column-preview-card-component/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ThreeComponent from "./ThreeComponent";
+
+function App() {
+  return (
+    <div className="general_component">
+      <ThreeComponent></ThreeComponent>
+    </div>
+  );
+}
+
+export default App;
